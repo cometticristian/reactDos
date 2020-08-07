4cf2a9712cc988c8238fa51b8070483b58201e0b
Cristian Cometti <cometticristian@gmail.com>
2020-08-07 10:21:10 -0300

    ready to start

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainNav from './components/MainNav'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    
+    <React.Fragment>
+    <body>
+
+	<div id="wrapper">
+		
+		<NavBar/>
+    <MainNav/>
+
+	</div>
+
+</body>
+</React.Fragment>
+
+  );
+}
+
+export default App;
